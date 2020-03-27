c2681548fb90733a0f2b28c549e8983484924fce
fnguler <59033367+fnguler@users.noreply.github.com>
2020-03-27 03:59:28 +0300

    Add files via upload
    The game has been remastered and replaced by the version "pitch game". All functions are deleted to be written again and variable names have changed. Some issues have been found.

--- blackjack.c
+++ blackjack.c
@@ -1,153 +1,144 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define ksayi 52
-int deste[ksayi] = {1,2,3,4,5,6,7,8,9,10,11,12,13,		//sinek
-			14,15,16,17,18,19,20,21,22,23,24,25,26,		//maça
-			27,28,29,30,31,32,33,34,35,36,37,38,39,		//karo
-			40,41,42,43,44,45,46,47,48,49,50,51,52};	//kupa
-int karisik[ksayi] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-int toplam=0;
-int numara;
-int deger=0;
+#define cnumber 52
+#define pit 104
+int deck1[cnumber] = {1,2,3,4,5,6,7,8,9,10,11,12,13,	//club
+			14,15,16,17,18,19,20,21,22,23,24,25,26,		//spade
+			27,28,29,30,31,32,33,34,35,36,37,38,39,		//diamond
+			40,41,42,43,44,45,46,47,48,49,50,51,52};	//heart
 
-void karistir ();
-void kartlari_yazdir(int birinci, int ikinci);
-void kart(int kart_num);
-void asiller (int num);
-void sayilar (int num);
-void total ();
-void toplam_yazdir ();
-void kart_sor();
-void kart_cek();
+int deck2[cnumber] = {1,2,3,4,5,6,7,8,9,10,11,12,13,	//club
+			14,15,16,17,18,19,20,21,22,23,24,25,26,		//spade
+			27,28,29,30,31,32,33,34,35,36,37,38,39,		//diamond
+			40,41,42,43,44,45,46,47,48,49,50,51,52};	//heart
+			
+struct plca{
+	int cards[15];
+	int total;
+};
+struct plca player[8];
+			
+int pitch[pit];
+int mixed[pit] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int j=0, cardnum, cardclass;
+
+void append ();
+void mix ();
+void drawbeg ();
+void addc ();
+void output();
+void decision ();
+void draw();
 
 int main(){
-	int sayac;
-	int birinci, ikinci;
-	int toplam1, toplam11;
 	
-	karistir();
-	birinci=karisik[0];
-	ikinci=karisik[1];
-	kartlari_yazdir(birinci, ikinci);
-	topl:
-	toplam_yazdir();
-	
-	if (toplam<21){
-		kart_sor();
-	}
-	else if (toplam==21){
-		printf ("Tebrikler!");
-	}
-	else{
-		printf ("Kaybettiniz.");
-	}
-	system ("pause");
-	return 0;
 }
-void karistir (){
+
+void append(){
+	int x;
+	for (x=0; x<pit; x++){
+		if (x<52){
+			pitch[x]=deck1[x];
+		}
+		else{
+			pitch[x]=deck2[x];
+		}
+	}
+}
+
+void mix (){
 	int i, random;
 	srand(time(0));
-	for (i=0; i<ksayi; i++){
+	for (i=0; i<pit; i++){
 		randgen:
-		random=rand()%52;
-		if (karisik[random]==0){
-			karisik[random]=deste[i];
+		random=rand()%104;
+		if (mixed[random]==0){
+			mixed[random]=pitch[i];
 			continue;
 		}
 		else 
 		goto randgen;
 	}
 }
-void kartlari_yazdir(int birinci, int ikinci){
-	printf ("Elinizdeki kartlar ");
-	printf (" %d %d ", birinci, ikinci);
-	kart (birinci);
-	printf (" ve ");
-	kart (ikinci);
-	printf ("\b.\n");
+
+void drawbeg (){
+	int i, k;
+	for (k=0; k<2; k++){
+		for (i=0; i<16; i++){
+			player[i].cards[k]=mixed[j];
+			j++;
+		}
+	}
 }
-void kart(int kart_num){
-	int sinif;
-	char isimbir[5]="sinek", isimiki[4]="maca", isimuc[4]="karo", isimdort[4]="kupa";
-	numara=kart_num%13;
-	sinif=kart_num/13+1;
-	if ((numara==0)&&(sinif!=1)){
-		sinif--;
+
+void addc (){
+	int l, p;
+	for (p=0; p<8; p++){
+		for (l=0; l<15; l++){
+			player[p].total=0;
+			player[p].total+=player[p].cards[l];
+		}	
 	}
-	else if (numara==0){
-		numara=13;
-	}
-	
-	
-	if ((numara>1)&&(numara<11)){
-		sayilar (numara);
-	}
-	else{
-		asiller (numara);
-	}	
+}
+
+void output(){
+	int q, y, z;
+	for (z=0; z<8; z++){
+		y=0;
+		do{
+			cardnum=player[z].cards[y]%13;
+			if (cardnum==0){
+				cardnum=13;
+			}
+			cardclass=(player[z].cards[y]/13)+1;
+			y++;
+		}while (player[z].cards[y]!=NULL);
+		
+		printf ("Number %d's cards are ", z);
+		for (q=0; q<y; q++){
+			if (cardnum==1){
+				printf ("A ");
+			}
+			else if (cardnum==11){
+				printf ("J ");
+			}
+			else if (cardnum==12){
+				printf ("Q ");
+			}
+			else if (cardnum==13){
+				printf ("K ");
+			}
+			else{
+				printf ("%d ", cardnum);
+			}
+			
+			if (cardclass==1){
+				printf ("club");
+			}
+			else if (cardclass==2){
+				printf ("spade");
+			}
+			else if (cardclass==3){
+				printf ("diamond");
+			}
+			else if (cardclass==4){
+				printf ("heart");
+			}
+			if ((y!=2)&&(q!=y-1)){
+				printf (", ");
+			}
+			else if ((y==2)&&(q==y-2)){
+				printf (" and ");
+			}
+		}
 		
 		
-	if (sinif==1){
-		printf ("(%s) ", isimbir);
 	}
-	else if (sinif==2){
-		printf ("(%s) ", isimiki);
-	}
-	else if (sinif==3){
-		printf ("(%s) ", isimuc);
-	}
-	else{
-		printf ("(%s) ", isimdort);
-	}	
+
+}
+
+void decision(){
 	
-	total(numara);
 }
 
-void asiller (int num){
-	if (num==1){
-		printf ("A");
-	}
-	else if (num==11){
-		printf ("J");
-	}
-	else if (num==12){
-		printf ("Q");
-	}
-	else if ((num==0)||(num==13)){
-		printf ("K");
-	}
-}
-
-void sayilar (int num){
-	printf ("%d", num);
-}
-
-void total(int numara){
-	int toplam1, toplam11;
-	if ((numara==0)||(numara==11)||(numara==12)||(numara==13)){
-		toplam+=10;
-	}
-	else if (numara!=1){
-		toplam+=numara;
-	}
-	else if (numara==1){
-		toplam+=11;
-		deger=1;
-	}
-}
-
-void toplam_yazdir(){
-	int toplam1, toplam11;
-	if((deger==1)&&(toplam<21)){
-		printf ("Elinizde as oldugu icin %d ve %d olmak uzere iki toplam elde edeceksiniz.", toplam-10, toplam);
-	}
-	else{
-		printf ("Elinizdeki kartlarin toplami %d.\n", toplam);
-	}
-}
-void kart_sor(){
-	int sent;
-	printf ("Kart cekmek istiyorsaniz -9'u tuslayiniz.");
-	scanf ("%d", &sent);
-}
